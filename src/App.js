472d35d2c13060266432a6f01f4d8e862d29b7d6
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-05-15 20:09:10 -0500

    Adding boxes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Header from './Components/Header';
+import Navigation from './Components/Navigation';
+import Main from './Components/Main';
+import SubContents from './Components/SubContents';
+import Advertisement from './Components/Advertisement';
+ 
+                
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Navigation />
+      <Main>
+        <SubContents />
+        <SubContents />
+        <SubContents />
+        <Advertisement />
+      </Main>
     </div>
   );
 }
-
+                
 export default App;
+
---
+++ src/Components/SubContents.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SubContents = () => {
+  return (
+    <div className='subContents'></div>
+  )
+}
+
+export default SubContents
